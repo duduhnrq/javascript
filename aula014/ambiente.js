7846599b0b2e7d4840b0898e294be42133240676
Eduardo Henrique <dudu30rafa@gmail.com>
2025-03-13 09:24:16 -0300

    praticando

---
+++ aula014/ambiente.js
@@ -0,0 +1,12 @@
+/*
+for (let c = 1; c <= 10; c++) {
+    console.log(`Passo ${c}`)
+}
+*/
+
+console.log('Vai começar...')
+for (let c = 1; c <= 10; c++) {
+    console.log(c)
+}
+
+console.log('Fim!')
