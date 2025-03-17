723f2661ee1217e5bd802205572493fe464909cf
Eduardo Henrique <dudu30rafa@gmail.com>
2025-03-17 14:59:07 -0300

    fazendo exercícios

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -2,10 +2,16 @@ function gerar() {
     let num = document.getElementById('txtn');
     let tab = document.getElementById('seltab');
     
-    if (num.value.length == 0) {
+    if (num.value.length <= 0) {
         arlet('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
-    }
 
+        for (let i = 0; i <= 10; i++) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${i} = ${n * i}`
+            item.value = `v${i}`;
+            tab.appendChild(item);
+        }
+    }
 }
